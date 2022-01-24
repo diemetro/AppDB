d17118f38557ba4a7cd5ddc453aff866dfd0526f
Dmitriy Plaschinskiy <it-admin@scancity.ru>
2022-01-24 20:48:06 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 cd ${DEPLOY_DIR}
 docker network create --driver overlay internal || true
 docker stack deploy --compose-file docker-compose.yaml mysql
+#envsubst < docker-compose.tpl > docker-compose.yaml
+cat docker-compose.yaml
 cd
 rm -rf ${DEPLOY_DIR}
 
