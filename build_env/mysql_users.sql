598a57cf3505a6b28e158c488391f1feae2eb7db
Dmitriy Plaschinskiy <it-admin@scancity.ru>
2022-01-25 20:08:44 +0000

    Add new file mysql_users.sql

---
+++ build_env/mysql_users.sql
@@ -0,0 +1,4 @@
+CREATE USER admitry@'%' IDENTIFIED WITH mysql_native_password BY '2Y#8U_ZbH97X';
+CREATE USER identity-svc@'%' IDENTIFIED WITH mysql_native_password BY 'uX$Z8C6L6pk';
+GRANT ALL PRIVILEGES ON identityservice.* TO admitry@'%';
+GRANT SELECT,INSERT,UPDATE,DELETE ON identityservice.* TO identity-svc@'%';
