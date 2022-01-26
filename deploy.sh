4b3aeb2c29159a8244e630a4fb91d86874de070a
Dmitriy Plaschinskiy <it-admin@scancity.ru>
2022-01-26 20:33:20 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd ${DEPLOY_DIR}
 docker network create --driver overlay internal-net-${CI_COMMIT_REF_NAME} || true
 envsubst < docker-compose.tpl > docker-compose.yaml
 cat docker-compose.yaml
-docker stack deploy --compose-file docker-compose.yaml mysql
+docker stack deploy --compose-file docker-compose.yaml mysql-${CI_COMMIT_REF_NAME}
 cd
 rm -rf ${DEPLOY_DIR}
 
